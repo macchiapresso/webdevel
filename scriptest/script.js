f2b7d98ee45125fe34b18eb1cace3d5c24059bf8
macchiapresso <cesargamer31@gmail.com>
2024-09-13 21:53:55 -0300

    start js learning + fancy flexbox stuff

---
+++ scriptest/script.js
@@ -0,0 +1,13 @@
+console.log("hello world :3")
+
+let number = 12
+console.log("original number is " + number)
+number = number + 5
+console.log("now it is " + number + " yippeee")
+
+const NUM = 15
+console.log("this number is constant and cannot be changed: " + NUM)
+
+console.log((4 + 6 + 9) / 77)
+console.log("the tetration of three: " + 3 ** (3 ** 3))
+console.log(0 / 0)
